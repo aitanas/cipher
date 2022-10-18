8ce67aef87fbcbff4e09f2061ccb4e2b28abab81
Aitana Shough <aeshough@gmail.com>
2022-10-18 14:05:42 -0700

    add HTML page with cipher.js linked
    Co-authored by: Richelle Thorpe <richelle.ak@gmail.com>

--- cipher.js
+++ cipher.js
@@ -1,18 +1,39 @@
 
-// 1. User enters sentence (sentence)
-// 2. Take first and last letters, capitalize them, return as new string (firstAndLast)
-// 3. Call other functions created 
-// 4. Concatenate the two-letter string with the original phrase, adding firstAndLast to end of sentence
-// 5. Count # of letters in original dentence, divide by 2, and output letter at this index. Concaenate at the beginning of the original sentence.
+// 1. f1: User enters sentence (sentence) 
+// 2. f2: Take first and last letters, capitalize them, return as new string (firstAndLast)
+// 3. f3: combine components: Concatenate the two-letter string with the original phrase, adding firstAndLast to end of sentence
+// 4. f4: Count # of letters in original sentence, divide by 2, and output letter at this index. Concaenate at the beginning of the original sentence.
 
-function encodeSentence () {
-  const sentence = window.prompt("Enter a sentence.");
-  window.alert(sentence);
-  let lastChar = sentence.length - 1
+const sentence = window.prompt("Enter a sentence.");
+window.alert(sentence);
+
+function lastTwo()  {
+  const lastChar = sentence.length - 1;
   let firstAndLast = sentence.charAt(lastChar) + sentence.charAt(0);
   firstAndLast = firstAndLast.toUpperCase();
-  const cipher = sentence + firstAndLast;
   return firstAndLast;
 }
 
-encodeSentence ();
+// lastTwo ();
+
+function combineComponents () {
+  firstAndLast = lastTwo();
+  const cipher = sentence + firstAndLast;
+  return cipher;
+}
+
+// combineComponents();
+
+function addMiddleLetter () {
+  const cipher = combineComponents();
+  sentenceLength = sentence.length; 
+  index = Math.floor(sentenceLength / 2);
+  finalCipher = sentence.charAt(index) + cipher;
+  return finalCipher
+}
+
+addMiddleLetter();
+
+// count sentence length... sentLength = sentence.length
+// divide sentLength by 2 - round down Math.floor = index
+// concatenate letter at index + cipher = finalCipher
